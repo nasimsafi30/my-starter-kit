0ac58ab4a03c7bed9fe0cc1dd3c1f3fd8f589f64
Mohammad Nasim Safi <nasim.safi30@gmail.com>
2026-07-19 10:55:54 +0430

    Fix CI linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,16 @@ const config = {
   rules: {
     "no-console": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { 
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+      caughtErrorsIgnorePattern: "^_"
+    }],
     "react-hooks/set-state-in-effect": "off",
-    "react-hooks/exhaustive-deps": "off"
+    "react-hooks/exhaustive-deps": "off",
+    "react/no-unescaped-entities": "off",
+    "prefer-const": "warn"
   },
 };
+
 module.exports = config;
