2b1364da5cdc3e4ff29a54700b416c5af63b4507
Mohammad Nasim Safi <nasim.safi30@gmail.com>
2026-07-19 13:55:41 +0430

    Remove TypeScript - rename tsconfig to disable

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" },
-    ],
-    unoptimized: true,
-  },
+  output: 'standalone',
+  images: { unoptimized: true },
   serverExternalPackages: ["bcryptjs"],
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
