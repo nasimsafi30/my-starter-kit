ff225afac6ced5af0a7338045a5bb2e568e099a5
Mohammad Nasim Safi <nasim.safi30@gmail.com>
2026-07-19 13:07:33 +0430

    Remove lint, clean files, fix CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ const config = {
   extends: ["next/core-web-vitals", "prettier"],
   rules: {
     "no-console": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-unsafe-function-type": "warn",
-    "react-hooks/set-state-in-effect": "warn",
-    "react-hooks/exhaustive-deps": "warn",
-    "react-hooks/immutability": "warn",
-    "react/no-unescaped-entities": "warn",
-    "prefer-const": "warn",
-    "@next/next/no-html-link-for-pages": "warn"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unsafe-function-type": "off",
+    "react-hooks/set-state-in-effect": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "react-hooks/immutability": "off",
+    "react/no-unescaped-entities": "off",
+    "prefer-const": "off",
+    "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off"
   },
 };
 
